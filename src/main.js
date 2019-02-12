6e72f782e352d4ba15e9de86ab85babc59e2a069
yj-shuang <497706870@qq.com>
2019-02-12 20:40:22 +0800

    css初始化文件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//引入 css 初始化文件
+import './css/common.css'
+
 //完整引入 mint-ui
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 //将mint-ui中的所有组件都注册到vue全局中
 Vue.use(MintUI)
-
-import App from './App'
-import router from './router'
 
 //引入MUI样式文件
 import './lib/mui/css/mui.min.css'
