a92a645016fb6f9eb31e8251cf96e1ad8353f51e
yj-shuang <497706870@qq.com>
2019-02-13 18:40:45 +0800

    将新闻列表页的每一项改造成路由链接,路由传递参数,传递每个新闻的id,在创建的新闻详情页接收参数

---
+++ src/pages/newsInfo/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="newsInfo-container">
+        <h1>新闻详情页面--{{id}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            id:this.$route.params.id
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import memberComponent from '../pages/member'
 import shopcarComponent from '../pages/shopcar'
 import searchComponent from '../pages/search'
 import newsListComponent from '../pages/newsList'
+import newsInfoComponent from '../pages/newsInfo'
 
 export default new Router({
   routes: [
@@ -18,6 +19,7 @@ export default new Router({
     { path: '/shopcar', component: shopcarComponent },
     { path: '/search', component: searchComponent },
     { path: '/home/newslist', component: newsListComponent },
+    { path: '/home/newsInfo/:id', component: newsInfoComponent },
   ],
   //将路由链接的点击高亮类名改为 mui-active , 因为引入了mui中的样式文件,其中就有mui-active的高亮样式,不需要我们自己额外写
   linkActiveClass:"mui-active"
