4fb633d5da63904b281097fbc14b80bd16e2c932
yj-shuang <497706870@qq.com>
2019-02-15 17:53:49 +0800

    完成图片详情页的图片列表功能,用到了图片懒加载

--- src/pages/photos/index.vue
+++ src/pages/photos/index.vue
@@ -4,27 +4,23 @@
         <div id="slider" class="mui-slider">
             <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
                 <div class="mui-scroll">
-                    <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-                        推荐
+                    <a :class="['mui-control-item',item.id == 0 ? 'mui-active':'']"  data-wid="tab-top-subpage-1.html" v-for="item in categories" :key="item.id" @click="getPhotoList(item.id)">
+                        {{item.title}}
                     </a>
-                    <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-                        热点
-                    </a>
-                    <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-                        北京
-                    </a>
-                    <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-                        社会
-                    </a>
-                    <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-                        娱乐
-                    </a>
-                    <!--<a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
-                        科技
-                    </a>-->
                 </div>
             </div>
         </div>
+
+        <!-- 图片列表 : 这里用到了图片懒加载技术,在 mint-ui 中有lazy-load组件,但是只有全局注册组件才有用 -->
+        <ul class="photoList">
+          <li v-for="(item,index) in list" :key="index">
+            <img v-lazy="item.img_url">
+            <div class="info">
+              <h1 class="info-title">{{item.title}}</h1>
+              <div class="info-content">{{item.zhaiyao}}</div>
+            </div>
+          </li>
+        </ul>
     </div>
 </template>
 <script>
@@ -52,20 +48,78 @@ mui('.mui-scroll-wrapper').scroll({
 export default {
   data(){
     return {
-
+      categories:[],
+      list:[]
     }
+  },
+  created(){
+    this.getCategories()
+    //一进入页面就要获取全部的图片列表,分类的id为0
+    this.getPhotoList(0)
   },
   mounted(){  //在 DOM元素全都加载完毕之后,再初始化组件
     //初始化scroll控件：
     mui('.mui-scroll-wrapper').scroll({
         deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     });
+  },
+  methods:{
+    getCategories(){  //获取顶部滑动区域的所有分类
+      this.$http.get('api/getimgcategory')
+      .then(result=>{
+        result.body.message.unshift({id:0,title:"全部"})
+        this.categories = result.body.message
+      })
+    },
+    getPhotoList(cateid){ //获取图片列表,根据顶部的分类获取
+      this.$http.get('api/getimages/'+cateid).then(result=>{
+        console.log(result.body.message);
+        this.list = result.body.message
+      })
+
+    }
   }
 }
 </script>
 <style lang="less" scoped>
   //让滑动更加流畅
   *{
-      touch-action: pan-x;
+      touch-action: pan-y;
   }
+  .photoList{
+    text-align: center;
+    padding: 0 4px;
+    li{
+      background-color: #ccc;
+      position: relative;
+      margin-top: 10px;
+      //图片懒加载中的图片样式
+      img{
+        width: 100%;
+        vertical-align: middle;
+      }
+      img[lazy=loading] {
+        width: 40px;
+        height: 300px;
+        margin: auto;
+      }
+      .info{
+        position: absolute;
+        text-align: left;
+        bottom: 0;
+        color: #fff;
+        max-height: 84px;
+        background-color: rgba(0,0,0,0.5);
+        .info-title{
+          font-size: 14px;
+          font-weight: 700;
+        }
+        .info-content{
+          font-size: 13px;
+        }
+      }
+    }
+    
+  }
+  
 </style>
