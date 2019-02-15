84b46c4481e2f1a221a5fdc3e1592d2f02be1cd0
yj-shuang <497706870@qq.com>
2019-02-15 11:08:02 +0800

    实现评论组件里面的加载更多评论和发表评论

--- src/components/comment.vue
+++ src/components/comment.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="comment-container">
-        <hr>
         <h1>发表评论</h1>
-        <textarea placeholder="最多BB120字" maxlength="120"></textarea>
-        <mt-button type="primary" size="large">发表评论</mt-button>
+        <hr>
+        <textarea placeholder="最多BB120字" maxlength="120" v-model="msg"></textarea>
+        <mt-button type="primary" size="large" @click="postComment">发表评论</mt-button>
         <div class="comment-content">
             <ul>
                 <li v-for="(item,index) in comments" :key="item.index">
@@ -12,20 +12,22 @@
                         <span>用户:{{item.user_name}}</span>
                         <span>发表时间:{{item.add_time | dateFormat}}</span>
                     </div>
-                    <div class="content">{{item.content}}</div>
+                    <div class="content">{{item.content == 'undefined'? '此用户很懒,没有评论': item.content}}</div>
                 </li>
             </ul>
         </div>
-        <mt-button type="danger" size="large" plain class="loadmore">加载更多</mt-button>
+        <mt-button type="danger" size="large" plain class="loadmore" @click="loadMore">加载更多</mt-button>
     </div>
 </template>
 
 <script>
+import {Toast} from 'mint-ui'
 export default {
     data(){
         return {
-            // id:this.$route.params
-            comments:[]
+            pageIndex:1,
+            comments:[],
+            msg:""
         }
     },
     props:["artid"],
@@ -33,11 +35,48 @@ export default {
         this.getComment()
     },
     methods:{
-        getComment(){
-            this.$http.get('api/getcomments/'+this.artid+'?pageindex=1').then(result=>{
-                console.log(result.body.message);
-                this.comments = result.body.message
+        getComment(){   //获取评论
+            this.$http.get('api/getcomments/'+this.artid+'?pageindex='+this.pageIndex).then(result=>{
+                // console.log(result.body.message);
+                // this.comments = result.body.message
+                //每当加载更多的时候,不要直接把之前的数据清空,而是将新数据跟老数据进行拼接
+                this.comments = this.comments.concat(result.body.message)
+            },err=>{
+                Toast("获取评论失败")
             })
+        },
+        loadMore(){ //加载更多评论
+            this.pageIndex++
+            this.getComment()
+        },
+        postComment(){  //发表评论
+            //1.把用户提交的内容提交给服务器-- v-model 双向数据绑定
+            // 提交的评论内容做非空校验
+            if(this.msg.trim().length == 0) return Toast("评论内容不能为空")
+            // 发送请求,提交内容
+                //post请求: this.$http.post(请求地址,[请求体参数],[配置])
+            this.$http.post('api/postcomment/'+this.artid,{content:this.msg})
+            .then(result=>{
+                console.log(result.body);
+                Toast("发表评论成功")
+                //2.将用户评论的内容放在一楼
+                //2.1方法1.手动拼一个数据,加到数据的最前面
+                /* this.comments.unshift({
+                    add_time:Date.now(),
+                    content:this.msg,
+                    user_name:"匿名用户"
+                })
+                this.msg="" */
+
+                //2.2 方法2 :重新调用getComment() , 这种方法在工作中使用较多,但是由于重新获取了评论,会闪一下
+                // 注意:原有数据需要清空,否则会出现重复拼接,还要将pageIndex重置为1
+                this.comments = []
+                this.pageIndex = 1
+                this.getComment()
+                this.msg = "" 
+            })
+            
+
         }
     }
 }
@@ -45,6 +84,10 @@ export default {
 
 <style lang="less" scoped>
     .comment-container{
+        h1{
+            font-size: 16px;
+            font-weight: 700;
+        }
         padding: 0 4px;
         .comment-content{
             margin-top: 15px;
@@ -56,11 +99,14 @@ export default {
                         border-radius: 2px;
                         margin-top: 10px;
                     }
+                    .content{
+                       text-indent: 32px; 
+                    }
                 }
             }
         }
         .loadmore{
-            margin-top: 15px;
+            margin: 15px 0;
         }
     }
 </style>
